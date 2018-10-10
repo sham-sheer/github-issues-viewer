877ee57f10ced1194fbc6e0a8d3344e0aea139fc
sham-sheer <Sameenhaja@Sameens-MacBook-Pro-4.local>
2018-10-10 15:26:41 -0700

    user-context

--- src/Components/user-context.js
+++ src/Components/user-context.js
@@ -2,6 +2,6 @@
 import React from 'react';
 
 export const UserContext = React.createContext({
-  username: 'sham-sheer',
-  password: '370ca3dcb3715e7fe66f9728f54a54ac5017e29c'
+  username: '',
+  password: ''
 })
